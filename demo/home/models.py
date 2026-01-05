cd8316fb947506212af09d48615837b7e0e50bca
Preksha <ranapreksha8@gmail.com>
2026-01-05 18:50:54 +0530

    Add Car model and migration

--- demo/home/models.py
+++ demo/home/models.py
@@ -9,5 +9,10 @@ class Student(models.Model):
     address = models.TextField()
     file = models.FileField()
 
-class Product(models.Model):
-    pass
+class Car(models.Model):
+    name = models.CharField(max_length=500)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    speed = models.IntegerField()
+
+    def __str__(self):
+        return self.name
